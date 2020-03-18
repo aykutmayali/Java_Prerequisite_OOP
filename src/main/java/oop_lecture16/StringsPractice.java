fce5d2c78794697815693caf356dcbb84cbeb132
aykutmayali <aykutmayali@gmail.com>
2020-03-18 17:31:59 +0300

    lecture 16
    String methods used

--- src/main/java/oop_lecture16/StringsPractice.java
+++ src/main/java/oop_lecture16/StringsPractice.java
@@ -23,5 +23,17 @@ public class StringsPractice {
         if(b.equalsIgnoreCase("There")){
             System.out.println("b.equals without capitalLetter 'There'");
         }
+
+        System.out.println(b.charAt(3)); //only 3. char displayed
+        System.out.println(a.charAt(2));
+        str="Hello there someone";
+        int strInt= str.indexOf("there");
+        System.out.println("starting place of 'there' in str is :"+ strInt);
+        str="Hello there someone there";
+        strInt=str.indexOf("there",8);
+        System.out.println("second 'there' place from 8.th char : "+strInt);
+        int c=str.lastIndexOf("there");
+        System.out.println(" address of 'there ' from reverse of sentence is "+c);
+
     }
 }
